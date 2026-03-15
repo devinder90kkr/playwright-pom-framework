19f22c000432d49aae94abd2e64796a136deb57b
Devinder Kumar <163675450+devinder90kkr@users.noreply.github.com>
2026-03-15 20:28:33 +0530

    Remove commented-out configurations for Firefox and WebKit from Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -41,17 +41,16 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    }
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 
 });
